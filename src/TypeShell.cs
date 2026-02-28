0f53577306cbe7c87595a4d88aa94a1b5ac37e6c
Cameron Day <32853335+ConanDW@users.noreply.github.com>
2026-02-28 15:41:02 -0500

    Forgot to upload csproj and New Shell

--- src/TypeShell.cs
+++ src/TypeShell.cs
@@ -1,9 +1,16 @@
+using System.Management.Automation;
 namespace TypeShell;
 
-public class Typeshell {
-    public static void Main(string[] args) {
+[Cmdlet(VerbsData.ConvertTo, "TypeShell")]
+public class Typeshell : PSCmdlet {
+    [Parameter(Mandatory = true, Position = 0)]
+    public string InputPath { get; set; }
+    
+    [Parameter(Mandatory = false, Position = 1)]
+    public string OutputPath { get; set; }
+    protected override void BeginProcessing() {
         Transpiler transpiler = new Transpiler();
-        string PowerShellCode = transpiler.Transpile(args[0]);
+        string PowerShellCode = transpiler.Transpile(InputPath);
         Console.WriteLine(PowerShellCode);
         //transpiler.WriteFile(args[1], PowerShellCode);
     }
